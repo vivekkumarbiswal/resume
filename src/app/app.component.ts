5c8c17d9ab80b7f52241b0f2a0f8637df4641764
vivekkumarbiswal <vivekkumarbiswal@gmail.com>
2026-04-09 00:09:21 +0530

    Resu,e

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'resume';
+  contactInfo = {
+    phone: '+91 8********0',
+    email: 'vivekkumarbiswal@gmail.com',
+    linkedin: 'https://www.linkedin.com/in/vivek-kumar-biswal-a84856196/',
+    github: 'https://github.com/vivekkumarbiswal',
+    location: 'Bangalore',
+  };
+
+  downloadResume() {
+    const link = document.createElement('a');
+    link.href = 'VivekKumarBiswal_FrontendDeveloper_4Years_Resume.pdf';
+    link.download = 'VivekKumarBiswal_FrontendDeveloper_4Years_Resume.pdf';
+    link.click();
+  }
 }
